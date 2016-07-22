e3d474e255ce35e1c024f66097b7799608132fa2
XMG <XMG@163.com>
2016-07-22 11:50:56 +0800

    修改了格式

--- MJExtensionExample/MJExtensionConfig.m
+++ MJExtensionExample/MJExtensionConfig.m
@@ -16,13 +16,17 @@
 #import "MJBook.h"
 
 @implementation MJExtensionConfig
+
 /**
  *  这个方法会在MJExtensionConfig加载进内存时调用一次
  */
+
 + (void)load
 {
+
 #pragma mark 如果使用NSObject来调用这些方法，代表所有继承自NSObject的类都会生效
 #pragma mark NSObject中的ID属性对应着字典中的id
+   
     [NSObject mj_setupReplacedKeyFromPropertyName:^NSDictionary *{
         return @{
                  @"ID" : @"id"
